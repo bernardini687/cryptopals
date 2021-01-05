0ed5820b05932323fe8cf4adb0474db00eb33086
oscar bernardini <bernardini687@gmail.com>
2021-01-05 12:05:38 +0100

    solve: set 1 challenge 1

--- src/c101.rs
+++ src/c101.rs
@@ -1,5 +1,13 @@
-pub fn solve(input: &str) -> &str {
-    input
+extern crate base64;
+extern crate hex;
+
+pub fn solve(input: &str) -> String {
+    let decoded_hex =
+        hex::decode(input.as_bytes()).expect("Error while decoding expected hex string");
+
+    println!("{:?}", decoded_hex);
+
+    base64::encode(decoded_hex)
 }
 
 #[cfg(test)]
@@ -10,12 +18,10 @@ mod test {
     #[test]
     fn convert_hex_to_base64() {
         let input = read_input("101");
+        let expected_output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
         dbg!(&input);
 
-        assert_eq!(
-            solve(&input),
-            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-        );
+        assert_eq!(solve(&input), expected_output);
     }
 }
