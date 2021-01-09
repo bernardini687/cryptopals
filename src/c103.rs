f00a1698e5c50c625c4465b71ca156bfd5e33186
oscar bernardini <bernardini687@gmail.com>
2021-01-09 19:00:43 +0100

    solve: quick and dirty set 1 challenge 3

--- src/c103.rs
+++ src/c103.rs
@@ -1,29 +1,38 @@
 #[cfg(test)]
 mod test {
-    use crate::utils::{decode_hex, encode_utf8, fixed_xor, read_input, repeat};
+    use crate::utils::{
+        decode_hex, encode_utf8, english_probability, fixed_xor, read_input, repeat,
+    };
+    use std::{collections::BTreeMap, iter::FromIterator};
 
-    fn decrypt(key_candidate: &str, input: &[u8]) {
+    fn decrypt(key_candidate: &str, input: &[u8]) -> String {
         let key = repeat(key_candidate, input.len()).into_bytes();
 
         let result = fixed_xor(&decode_hex(input), &key);
 
-        // eval against score
-
-        println!("{}", encode_utf8(result.as_slice()));
-
-        // { [score]: text }
+        encode_utf8(result.as_slice()).to_string()
     }
 
     #[test]
     fn single_byte_xor_cipher() {
         let input = read_input("103");
 
+        let mut scores = BTreeMap::new();
+
         // for char in ascii_set
+        for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".chars() {
+            let secret = letter.to_string();
+            let plaintext = decrypt(&secret, input.as_bytes());
+            let score = english_probability(&plaintext);
 
-        decrypt("a", input.as_bytes());
+            scores.insert(secret, score);
+        }
 
-        // print first 5 candidates sorted desc by the [score] key
+        let mut v = Vec::from_iter(scores);
+        v.sort_by(|&(_, a), &(_, b)| b.partial_cmp(&a).unwrap());
+
+        println!("{:?}", v[0]); // X is the secret!
+        println!("{:?}", v[1]);
+        println!("{:?}", v[2]);
     }
-
-    // etaoin shrdlu
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,7 @@
 extern crate base64;
 extern crate hex;
 
+use std::collections::BTreeMap;
 use std::{fs, iter, str};
 
 pub fn decode_hex(bytes: &[u8]) -> Vec<u8> {
@@ -32,10 +33,68 @@ pub fn repeat(data: &str, times: usize) -> String {
     iter::repeat(data).take(times).collect::<String>()
 }
 
-pub fn score(data: &str) -> usize {
-    // 1300 910 820 750 700 670 630 610 600 430 280
+/// Uses the Bhattacharyya coefficient to determine if text is likely to be English.
+///
+/// Higher is better.
+///
+/// (courtesy of https://github.com/elasticdog)
+pub fn english_probability(text: &str) -> f64 {
+    // count of the number of times a character occurs in the given text
+    let mut letter_frequencies = BTreeMap::new();
 
-    // etaoin shrdlu ->
+    for letter in text.to_uppercase().chars() {
+        let frequency = letter_frequencies.entry(letter).or_insert(0.0);
+        *frequency += 1.0;
+    }
 
-    todo!()
+    println!("{:?}", letter_frequencies);
+
+    // total number of characters in the given text
+    let total_letters = text.len() as f64;
+
+    // relative frequency of letters in the English language
+    let mut english_frequencies = BTreeMap::new();
+    english_frequencies.insert('A', 0.0651738);
+    english_frequencies.insert('B', 0.0124248);
+    english_frequencies.insert('C', 0.0217339);
+    english_frequencies.insert('D', 0.0349835);
+    english_frequencies.insert('E', 0.1041442);
+    english_frequencies.insert('F', 0.0197881);
+    english_frequencies.insert('G', 0.0158610);
+    english_frequencies.insert('H', 0.0492888);
+    english_frequencies.insert('I', 0.0558094);
+    english_frequencies.insert('J', 0.0009033);
+    english_frequencies.insert('K', 0.0050529);
+    english_frequencies.insert('L', 0.0331490);
+    english_frequencies.insert('M', 0.0202124);
+    english_frequencies.insert('N', 0.0564513);
+    english_frequencies.insert('O', 0.0596302);
+    english_frequencies.insert('P', 0.0137645);
+    english_frequencies.insert('Q', 0.0008606);
+    english_frequencies.insert('R', 0.0497563);
+    english_frequencies.insert('S', 0.0515760);
+    english_frequencies.insert('T', 0.0729357);
+    english_frequencies.insert('U', 0.0225134);
+    english_frequencies.insert('V', 0.0082903);
+    english_frequencies.insert('W', 0.0171272);
+    english_frequencies.insert('X', 0.0013692);
+    english_frequencies.insert('Y', 0.0145984);
+    english_frequencies.insert('Z', 0.0007836);
+    english_frequencies.insert(' ', 0.1918182);
+
+    // update the counts to be the relative frequency of letters in the given text
+    // and then calculate the Bhattacharyya coefficient as our score
+    let mut score = 0.0;
+
+    for letter in english_frequencies.keys() {
+        let relative_frequency = letter_frequencies.entry(*letter).or_insert(0.0);
+        *relative_frequency /= total_letters;
+
+        let partition_overlap = letter_frequencies[letter] * english_frequencies[letter];
+        score += partition_overlap.sqrt();
+    }
+
+    println!("{}", &score);
+
+    score
 }
